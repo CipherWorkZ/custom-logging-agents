2249a44cff451c50701a59a7c2bad27ca6f17f0f
CipherWorkZ <patrick-lacasse@outlook.com>
2025-09-19 10:02:21 -0400

    Update npm_monitor.py

--- nginx-reverse-proxy/npm_monitor.py
+++ nginx-reverse-proxy/npm_monitor.py
@@ -4,7 +4,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 CONFIG_FILE = "/etc/npm_monitor.conf"
-LOG_FILE = "/var/log/npm_monitor.log"3
+LOG_FILE = "/var/log/npm_monitor.log"
 AGENT_VERSION = "1.0.1"
 
 
